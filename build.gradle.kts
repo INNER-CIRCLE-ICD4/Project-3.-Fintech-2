2a1316cdfd98b3ed62501eb36629ac3986496c15
echo-youn <victoryjay727@gmail.com>
2025-07-22 21:51:29 +0900

    migrate kotlin add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 plugins {
-	java
+	kotlin("jvm") version "2.0.21"
+	kotlin("plugin.spring") version "2.0.21"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "2.0.21"
+	kotlin("plugin.allopen") version "2.0.21"
+	id("io.gitlab.arturbosch.detekt") version("1.23.8")
 }
 
 group = "fintech2"
 version = "0.0.1-SNAPSHOT"
+
+detekt {
+	toolVersion = "1.23.8"
+	config.setFrom(file("config/detekt/detekt.yml"))
+	buildUponDefaultConfig = true
+}
 
 java {
 	toolchain {
@@ -32,6 +42,8 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// detekt ktlint
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
 }
 
 tasks.withType<Test> {
---
+++ src/main/kotlin/fintech/easypay/EasypayApplication.kt
@@ -0,0 +1,11 @@
+package fintech.easypay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasypayApplication
+
+fun main(args: Array<String>) {
+    runApplication<EasypayApplication>()
+}
